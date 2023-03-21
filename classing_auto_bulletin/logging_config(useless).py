4fe0aef1d6f01864866111058ede7dfa4f6433d8
LABARBE Léo <labarbe.leo2308@gmail.com>
2023-03-21 19:14:19 +0100

    Suppression de fichiers
    None

--- classing_auto_bulletin/logging_config(useless).py
+++
@@ -1,4 +0,0 @@
-import logging
-import os
-
-logging.basicConfig(filename='LogsData/errors.log', encoding='utf-8', format='%(levelname)s : %(message)s (file : %(filename)s - line : %(lineno)d)', level=logging.ERROR)
